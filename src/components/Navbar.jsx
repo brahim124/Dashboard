1a1e380a99f88f874ba4c42ff6a1ef72e52cf59c
ibracoachs <brahimsrihi01@gmail.com>
2024-05-02 17:06:16 +0200

    créer notification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
-import { Cart, Chat, Notification,UserProfile } from '.';
+import { Chat, Notification,UserProfile } from '.';
 import {AppContext} from '../contexts/contextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
