60500688652c7f4326bd28e9471433845e8016fa
ibracoachs <brahimsrihi01@gmail.com>
2024-05-02 11:45:28 +0200

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,16 @@
+export { default as Button } from './Button';
+export { default as ThemeSettings } from './themeSettings';
+export { default as Sidebar } from './Sidebar';
+// eslint-disable-next-line import/no-cycle
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './footer';
+export { default as Cart } from './cart';
+export { default as Chat } from './chat';
+export { default as Notification } from './notification';
+export { default as UserProfile } from './userProfile';
+export { default as SparkLine } from './Charts/SparkLine';
+export { default as LineChart } from './Charts/LineChart';
+export { default as Stacked } from './Charts/stacked';
+export { default as Pie } from './Charts/pie';
+export { default as ChartsHeader } from './chartsHeader';
+export { default as Header } from './header';
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+export { default as Acceuil } from './Acceuil';
+export {default as Clients } from './Clients';
+export { default as Employees } from './Coachs';
